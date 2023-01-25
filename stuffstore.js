77cc756a4fab069612152435359fb8dacc77beb4
Artur Kordzik <akordzik@outlook.com>
2023-01-25 15:50:16 +0100

    draft

--- stuffstore.js
+++ stuffstore.js
@@ -1,37 +1,46 @@
 const fs = require('fs');
-const url = 'url.json';
-const data = fs.readFileSync(url, 'utf8');
-const jsonObject = JSON.parse(data);
 
-// try {
-//   fs.writeFileSync("test.txt", jsonObject.url);
-// } catch (err) {
-//   console.error(err);
-// }
+// const run = async () => {
+// 	// The API call
+// 	await fetch(jsonObject.url)
+// 		.then((response) => response.text())
+
+// 		// This is the HTML from our response as a text string
+// 		.then((html) => {
+// 			return html;
+// 		})
+
+// 		//saving html to index.html
+// 		.then((html) => {
+// 			fs.writeFileSync('index.html', html);
+// 		})
+
+// 		// If there was an error
+// 		.catch((err) => {
+// 			console.warn('Something went wrong.', err);
+// 		});
+// };
+
+const getHtml = async (url) => {
+	// use fetch and await response
+	return 'html...';
+};
+
+const handleHtml = async (html) => {
+	// do smth with fetched html
+};
 
 const run = async () => {
-	// The API call
-	await fetch(jsonObject.url)
-		.then((response) => response.text())
+	const url = 'url.json';
+	const data = fs.readFileSync(url, 'utf8');
+	const jsonObject = JSON.parse(data);
 
-		// This is the HTML from our response as a text string
-		.then((html) => {
-			return html;
-		})
-
-		//saving html to index.html
-		.then((html) => {
-			fs.writeFileSync('index.html', html);
-		})
-
-		// If there was an error
-		.catch((err) => {
-			console.warn('Something went wrong.', err);
-		});
+	const html = await getHtml(jsonObject.url);
+	await handleHtml(html);
 };
 
 (async () => {
-  console.log('Started execution');
-  await run();
-  console.log('Finished execution');
+	console.log('Started execution');
+	await run();
+	console.log('Finished execution');
 })();
