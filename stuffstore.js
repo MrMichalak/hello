1451bd76979bb2263e60d6a6f993927346f7afd2
Artur Kordzik <akordzik@outlook.com>
2023-01-25 14:13:31 +0100

    refactor

--- stuffstore.js
+++ stuffstore.js
@@ -1,29 +1,37 @@
-const fs = require("fs");
-const url = "url.json";
-const data = fs.readFileSync(url, "utf8");
+const fs = require('fs');
+const url = 'url.json';
+const data = fs.readFileSync(url, 'utf8');
 const jsonObject = JSON.parse(data);
 
-try {
-  fs.writeFileSync("test.txt", jsonObject.url);
-} catch (err) {
-  console.error(err);
-}
+// try {
+//   fs.writeFileSync("test.txt", jsonObject.url);
+// } catch (err) {
+//   console.error(err);
+// }
 
-// The API call
-fetch(jsonObject.url)
-  .then((response) => response.text())
+const run = async () => {
+	// The API call
+	await fetch(jsonObject.url)
+		.then((response) => response.text())
 
-  // This is the HTML from our response as a text string
-  .then((html) => {
-    return html;
-  })
+		// This is the HTML from our response as a text string
+		.then((html) => {
+			return html;
+		})
 
-  //saving html to index.html
-  .then((html) => {
-    fs.writeFileSync("index.html", html);
-  })
+		//saving html to index.html
+		.then((html) => {
+			fs.writeFileSync('index.html', html);
+		})
 
-  // If there was an error
-  .catch((err) => {
-    console.warn("Something went wrong.", err);
-  });
+		// If there was an error
+		.catch((err) => {
+			console.warn('Something went wrong.', err);
+		});
+};
+
+(async () => {
+  console.log('Started execution');
+  await run();
+  console.log('Finished execution');
+})();
